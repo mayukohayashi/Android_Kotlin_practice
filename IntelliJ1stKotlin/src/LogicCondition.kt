381a463728398438bc9ce3cfaad129e79b37a2e3
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-07-22 18:20:58 +0900

    learning: logic condition

--- IntelliJ1stKotlin/src/LogicCondition.kt
+++ IntelliJ1stKotlin/src/LogicCondition.kt
@@ -1,3 +1,39 @@
 fun main() {
-    println(3>2)
+    /*
+    >
+    <
+    >=
+    <=
+    !=
+    ==
+And Gate (&&)
+    True && True = True
+    True && False = False
+    False && True = False
+    False && False = False
+
+OR Gate (||)
+    True && True = True
+    True && False = True
+    False && True = True
+    False && False = False
+
+Not gate (!)
+    True = False
+    False = True
+     */
+
+    var condition: Boolean = (3 < 2)
+    println(condition)
+
+    val number = 11
+    println(number >= 1 && number <= 10)
+
+    println(number >= 1 || number <= 10)
+
+
+    val age = 23
+    println( age >= 60 || age <= 18)
+    println( !(age==22))
+
 }
