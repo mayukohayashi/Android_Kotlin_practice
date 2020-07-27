a86abdea8aff3e905d6f291781c689b34a56c57a
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-07-27 15:32:59 +0900

    learning: Array list

---
+++ IntelliJ1stKotlin/src/ArrayList.kt
@@ -0,0 +1,17 @@
+fun main() {
+    println("Enter number of Pets")
+    val maxSize:Int = readLine()!!.toInt()
+
+    // Write App in Array
+    var listOfPets = ArrayList<String>()
+
+    for (i:Int in 0 until maxSize) {
+        println("Enter Pet Name $i")
+        listOfPets.add(readLine()!!.toString())
+    }
+
+    for(i:Int in 0 until listOfPets.size) {
+        println("Pet $i: ${listOfPets[i]}")
+    }
+
+}
