26e02a17c372ba436fd0ad39cefeb17523aa97b5
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-07-31 18:46:08 +0900

    learning: Delegate

--- IntelliJ1stKotlin/src/com/car/maintain/CreditCardRating.kt
+++ IntelliJ1stKotlin/src/com/car/maintain/CreditCardRating.kt
@@ -28,10 +28,27 @@ class Visa(override val cardNumber: String) :CreditCard {
     }
 }
 
+// Delegate: clientはMasterCardでもVisaでもどちらでも対応できる
+class PayPal(client:CreditCard): CreditCard by client {
+
+    fun print(){
+        println(this.cardNumber)
+    }
+}
+
 fun main() {
     val visa = Visa("9279382781297312638292")
     visa.score(70)
 
     val masterCard = MasterCard("202372047282928")
     masterCard.score(73)
+
+    // Delegate使ったPaypal呼び出す
+    val payPalVisa = PayPal(visa)
+    println("card number Paypal: ${payPalVisa.cardNumber}")
+    payPalVisa.print()
+
+    val payPalMasterCard = PayPal(masterCard)
+    println("card number Paypal: ${payPalMasterCard.cardNumber}")
+    payPalMasterCard.print()
 }
