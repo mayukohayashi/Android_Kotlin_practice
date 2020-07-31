b43f41d1cbf37bb588b516a760791c837cb84bd6
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-07-31 17:11:55 +0900

    learning: data class

--- IntelliJ1stKotlin/src/com/car/maintain/DataClass.kt
+++ IntelliJ1stKotlin/src/com/car/maintain/DataClass.kt
@@ -1,2 +1,12 @@
 package com.car.maintain
 
+data class Person(val name:String, val age:Int, val gender:String)
+
+fun main() {
+
+    val person = Person("Mayuko", 22, "female")
+    println("person.name: ${person.name}")
+    println("person.age: ${person.age}")
+    println("person.gender: ${person.gender}")
+
+}
