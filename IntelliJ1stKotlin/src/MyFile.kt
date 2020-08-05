e75a816282b2ade4ce540e5a897da3b111a49720
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-08-05 14:55:31 +0900

    learning: file read

--- IntelliJ1stKotlin/src/MyFile.kt
+++ IntelliJ1stKotlin/src/MyFile.kt
@@ -3,16 +3,30 @@ import java.io.FileWriter
 
 fun main(args:Array<String>) {
 
-    println("Write something to Hello.txt")
-    var str:String = readLine()!!.toString()
-    WriteToFile(str)
+    println("1: Read\n2: Write \n")
+    val operation = readLine()!!.toInt()
+
+    when(operation) {
+        1 -> {
+            ReadFromFile()
+        }
+
+        2 -> {
+            ReadFromFile()
+            println("Write something to Hello.txt")
+            var str:String = readLine()!!.toString()
+            WriteToFile(str)
+        }
+    }
 }
 
 fun WriteToFile(str:String){
    try {
-        var findOut = FileWriter("Hello.txt", true) // append:trueで追加
-        findOut.write(str + "\n")
-        findOut.close()
+       val findOut = FileWriter("Hello.txt", true) // append:trueで追加
+       findOut.write(str + "\n")
+       findOut.close()
+       println("data is saved")
+
    } catch (ex:Exception) {
         println(ex.message)
    }
@@ -21,11 +35,12 @@ fun WriteToFile(str:String){
 
 fun ReadFromFile() {
     try {
-        var fileRead = FileReader("Hello.txt")
+        val fileRead = FileReader("Hello.txt")
         var character:Int?
         do {
             character = fileRead.read()
-        } while ()
+            print(character.toChar()) // printlnにすると１文字１行に
+        } while (character!=-1)
 
     } catch (ex:Exception) {
         println(ex.message)
