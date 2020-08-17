ce146ccbb7ae596572ba1a8e7e539d4f450c944c
林真悠子 <mayuko.hayashi@tongullman.co.jp>
2020-08-17 11:12:25 +0900

    Add: before holiday

--- FindMyAge/app/src/main/java/com/konsaiGobo/findmyage/MainActivity.kt
+++ FindMyAge/app/src/main/java/com/konsaiGobo/findmyage/MainActivity.kt
@@ -2,10 +2,23 @@ package com.konsaiGobo.findmyage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btGetAge.setOnClickListener {
+
+            // ここにボタンをクリックしたときに走るCode
+
+            val userDataOfTheBirth = txtDataOfBirth.text.toString()
+                // txtDataOfBirthの中のテキストをStringにしたもの
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+                // 今年をカレンダーから持ってくる
+
+        }
     }
 }
