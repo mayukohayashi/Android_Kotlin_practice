763c47ec9c96419d67e109f46673ac824d035355
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-08-17 14:23:45 +0900

    Done: Find my age app

--- FindMyAge/app/src/main/java/com/konsaiGobo/findmyage/MainActivity.kt
+++ FindMyAge/app/src/main/java/com/konsaiGobo/findmyage/MainActivity.kt
@@ -2,6 +2,7 @@ package com.konsaiGobo.findmyage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -11,18 +12,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btGetAge.setOnClickListener {
-
-            // ここにボタンをクリックしたときに走るCode
-
-            val userDataOfBirth = Integer.parseInt(txtDataOfBirth.text.toString())
-                // txtDataOfBirthの中のテキストをStringにしたものをIntegerに。
-
-            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
-                // 今年をカレンダーから持ってくる
-
-            val userAge = currentYear - userDataOfBirth
-                // 現在のYearからユーザーの誕生年を引いて、年齢を出す
-
+            buttonClickGetAge()
         }
+    }
+
+
+    fun buttonClickGetAge() {
+        // ここにボタンをクリックしたときに走るCode
+
+        val userDataOfBirth = Integer.parseInt(txtDataOfBirth.text.toString())
+        // txtDataOfBirthの中のテキストをStringにしたものをIntegerに。
+
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        // 今年をカレンダーから持ってくる
+
+        val userAge = currentYear - userDataOfBirth
+        // 現在のYearからユーザーの誕生年を引いて、年齢を出す
+
+        tvShowAge.text = "You are $userAge years old!"
+
+        // ちょっとログの出し方練習
+        Log.d("LogME!", "You are $userAge years old!")
     }
 }
